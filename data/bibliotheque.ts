2a4e59f66a22e829112e44e1ac3ac53901825765
Leonie <82599224+LeonieGoeb@users.noreply.github.com>
2026-04-15 00:02:39 +0200

    the happy prince supprimer

--- data/bibliotheque.ts
+++ data/bibliotheque.ts
@@ -1,10 +1,8 @@
 import { Livre } from './types';
 import annaKarenine from './anna-karenine';
 import laCiudadDeVapor from './la-ciudad-de-vapor';
-import theHappyPrince from './the-happy-prince';
 
 export const BIBLIOTHEQUE: Livre[] = [
   annaKarenine,
   laCiudadDeVapor,
-  theHappyPrince,
 ];
